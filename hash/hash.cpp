8947bd1e5065e25ed7e730b8688a0362ebfce67a
vernik03 <vernik03@gmail.com>
2022-02-09 21:52:18 +0200

    better output

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -7,6 +7,50 @@
 
 using namespace std;
 
+class SecondLevel {
+public:
+	SecondLevel(){}
+	SecondLevel(int p) {
+		P = p;
+	}
+	void AddElem(string temp) {
+		if (elems.size() > 1)
+		{
+			count_elems = elems.size()* elems.size();
+			A = rand();
+			B = rand();
+			int h=1;
+			for (int i = 0; i < temp.size(); i++)
+			{
+				h += ((A * temp[i] + B) % P) % count_elems;
+			}
+			if (h > elems.size())
+			{
+				elems.resize(h+1);				
+			}
+			elems[h] = temp;
+		}
+		else
+		{
+			elems.push_back(temp);
+		}		
+	}
+	void PrintTable() {
+		for (int i = 0; i < elems.size(); i++)
+		{
+			if (elems[i].size()>0)
+			{
+				cout << i << "|" << elems[i] << " ";
+			}
+			
+		}
+	}
+private:
+	int A, B, count_elems, P;
+	vector<string> elems;
+
+};
+
 class HashTable
 {
 public:
@@ -19,25 +63,56 @@ public:
 			in >> elem;
 			count_elems++;
 		}
-		cout << count_elems;
+		in.close();
+		hash_table.resize(count_elems);
+		A = rand();
+		B = rand();
+		for (int i = 0; i < count_elems; i++)
+		{
+			SecondLevel temp = (P);
+			hash_table[i] = temp;
+		}
 		ReadElem(elem, filename);
 	}
 
-	void ReadElem(string& elem, const string& filename) {
-		ifstream in(filename);
-		in >> elem;
-		FirstLevelHash(elem);
-	}
-
-	void FirstLevelHash(string& elem) {
-		int k;
+	void PrintTable() {
+		for (int i = 0; i < hash_table.size(); i++)
+		{
+			cout << i << ": ";
+			hash_table[i].PrintTable();
+			cout << endl;
+		}
 	}
 
 private:
-	vector<vector<string>> hash_table;
+	vector<SecondLevel> hash_table;
 	int file_size;
-	int count_elems = 0;
-	int A, B;
+	int count_elems = 0; //N
+	int A, B, P = 999;
+
+	void ReadElem(string& elem, const string& filename) {
+		ifstream in(filename);
+		while (!in.eof())
+		{
+			in >> elem;
+			FirstLevelHash(elem);
+		}
+	}
+	
+	void FirstLevelHash(string& elem) {
+		int h=1;
+		for (int i = 0; i < elem.size(); i++)
+		{
+			h += (((A * elem[i] + B) % P) % count_elems)/10;
+		}
+		if (h > hash_table.size())
+		{
+			hash_table.resize(h + 1);
+		}
+		hash_table[h].AddElem(elem);
+		
+	}
+
 };
 
 //В хэш - функции вида «(A * k + B) % P % N», с возможностью подбора двух коэффициентов A и В :
@@ -48,4 +123,5 @@ private:
 int main()
 {
 	HashTable table;
+	table.PrintTable();
 }
